2cc8ab9f6fae312b082ba859bfe6f92e8908acdf
Kyle Gann <kylegann@gmail.com>
2015-05-11 09:09:35 -0400

    Yosemite defaults correction

--- defaults.sh
+++ defaults.sh
@@ -1,5 +1,5 @@
 defaults write -g InitialKeyRepeat -int 25
 defaults write -g KeyRepeat -int 2
 
-defaults write com.apple.Dock autohide-delay -float 0
-defaults write com.apple.Dock autohide-time-modifier -float 0.5
+defaults write com.apple.dock autohide-delay -float 0
+defaults write com.apple.dock autohide-time-modifier -float 0.5
